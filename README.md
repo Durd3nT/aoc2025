9be759d80638bc2c070bfab46056ddbf1975cda2
Alexander Miessen <alexandermiessen@gmail.com>
2026-01-30 12:28:45 +0100

    c++ day 3

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ### Advent of Code 2025
 
 My solutions to [Advent of Code 2025](https://adventofcode.com/2025) - in Python and C++.
-In some cases, the Python code is used to prototype and get a results simply by solving the puzzle without optimizing code.
+In some cases, the Python code is used to prototype and get a result simply by solving the puzzle, without too much code optimization.
+
+#### C++:
+To compile and run, include the number of the day as a command line argument, e.g., `make run NUM="02"` for day 2.
 
 **NOTE:** the input is user-/account-dependent.
---
+++ cpp/day03.cpp
@@ -0,0 +1,76 @@
+#include <print>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <ranges>
+
+
+void read_input(const std::string filepath, std::vector<std::string> & data) {
+    std::ifstream inFile(filepath);
+    std::string val;
+
+    if (inFile.is_open()) {
+        while (getline(inFile, val)) {
+            data.push_back(val);
+        }
+    } else { std::println("ERROR: could not open file"); }
+}
+
+/**
+ * In a string of digits, finds the digits that will yield the largest possible integer when
+ * combined. Note that the order of digits cannot be changed. For example, in 818181911112111,
+ * the largest combination of three digits is 921 when respecting the order of digits (i.e.,
+ * disallowing 988 as the 8s would have to moved to the right of the 9)
+ * 
+ * @param b_string: string of digits
+ * @param num_digits: How many digits to maximize
+ */
+int64_t max_digits_string(std::string_view s, const int num_digits) {
+    int len_s = s.length();
+    std::vector<char> max_digits(num_digits);
+    max_digits[0] = s[0];
+    std::vector<int> max_idx(num_digits, 0);
+
+    for (const int k: std::views::iota(0, num_digits)) {
+        int start;
+        if (k == 0) {
+            start = max_idx[0] + 1;
+        } else {
+            start = max_idx[k - 1] + 1;
+        }
+        for (const int i: std::views::iota(start, len_s - num_digits + k + 1)) {
+            if (s[i] > max_digits[k]) {
+                max_digits[k] = s[i];
+                max_idx[k] = i;
+            }
+        }
+    }
+
+    int64_t max_int = 0;
+    for (char c: max_digits) {
+        max_int = max_int * 10 + (c - '0');
+    }
+
+    return max_int;
+}
+
+int64_t total_jolts(const std::vector<std::string> & data, const int num_digits) {
+    int64_t jolts = 0;
+    for (auto s: data) {
+        jolts += max_digits_string(std::string_view(s), num_digits);
+    }
+    return jolts;
+}
+
+
+int main() {
+    std::string filepath = "../input/input03.txt";
+    std::vector<std::string> data;
+
+    read_input(filepath, data);
+    int64_t jolts = total_jolts(data, 2);
+    int64_t big_jolts = total_jolts(data, 12);
+
+    std::println("maximum joltage with 2 batteries (part 1): {}", jolts);
+    std::println("maximum joltage with 12 batteries (part 2): {}", big_jolts);
+}
