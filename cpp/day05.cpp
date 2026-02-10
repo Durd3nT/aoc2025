0f00bcd3b86187d821ae6b226206227dae2142c6
Alexander Miessen <alexandermiessen@gmail.com>
2026-02-10 18:42:16 +0100

    day 5 c++ complete

--- cpp/day05.cpp
+++ cpp/day05.cpp
@@ -11,7 +11,11 @@
 #include "aoc_utils.hpp"
 
 
-void read_input(const std::string filepath, std::vector<std::vector<int>> & ranges, std::vector<int> & ids) {
+void read_input(
+    const std::string filepath,
+    std::vector<std::vector<int64_t>> & ranges,
+    std::vector<int64_t> & ids
+) {
     std::ifstream inFile(filepath);
 
     if (inFile.is_open()) {
@@ -24,9 +28,9 @@ void read_input(const std::string filepath, std::vector<std::vector<int>> & rang
             }
             if (not passed) {
                 std::vector<std::string> range = split(val, "-");
-                ranges.push_back(std::vector<int>{std::stoi(range[0]), std::stoi(range[1])});
+                ranges.push_back(std::vector<int64_t>{std::stoll(range[0]), std::stoll(range[1])});
             } else {
-                ids.push_back(std::stoi(val));
+                ids.push_back(std::stoll(val));
             }
         }
 
@@ -40,12 +44,12 @@ int random_int(int min, int max) {
 }
 
 size_t hoare_partition(
-    const std::vector<int> & A,
+    const std::vector<int64_t> & A,
     std::vector<size_t> & idx,
     size_t low,
     size_t high
 ) {
-    int pivot = A[idx[random_int(low, high)]];
+    int64_t pivot = A[idx[random_int(low, high)]];
     std::ptrdiff_t i = static_cast<std::ptrdiff_t>(low) - 1;
     std::ptrdiff_t j = static_cast<std::ptrdiff_t>(high) + 1;
 
@@ -65,7 +69,7 @@ size_t hoare_partition(
 }
 
 void quicksort(
-    const std::vector<int> & A,
+    const std::vector<int64_t> & A,
     std::vector<size_t> & idx,
     size_t low,
     size_t high
@@ -78,12 +82,12 @@ void quicksort(
     }
 }
 
-std::vector<int> sort_vector(const std::vector<int> & A) {
+std::vector<int64_t> sort_vector(const std::vector<int64_t> & A) {
     std::vector<size_t> idx(A.size());
     std::iota(idx.begin(), idx.end(), 0);
     quicksort(A, idx, 0, A.size() - 1);
 
-    std::vector<int> sorted;
+    std::vector<int64_t> sorted;
     for (const auto i: idx) {
         sorted.push_back(A[i]);
     }
@@ -92,10 +96,10 @@ std::vector<int> sort_vector(const std::vector<int> & A) {
 }
 
 void sort_combine_ranges(
-    const std::vector<std::vector<int>> & ranges,
-    std::vector<std::vector<int>> & combined_ranges
+    const std::vector<std::vector<int64_t>> & ranges,
+    std::vector<std::vector<int64_t>> & combined_ranges
 ) {
-    std::vector<int> range_starts;
+    std::vector<int64_t> range_starts;
     std::vector<size_t> idx(ranges.size());
     std::iota(idx.begin(), idx.end(), 0);
     for (const auto & r: ranges) {
@@ -117,40 +121,56 @@ void sort_combine_ranges(
 /**
  * 
  */
-void filter_valid_ids(
-    const std::vector<std::vector<int>> & sorted_ranges,
-    const std::vector<int> & ids
+int64_t filter_valid_ids(
+    const std::vector<std::vector<int64_t>> & sorted_ranges,
+    const std::vector<int64_t> & sorted_ids
 ) {
+    int64_t num_valid_ids = 0;
+
+    size_t id_cnt = 0;
+    size_t range_cnt = 0;
+
+    while (id_cnt < sorted_ids.size() and range_cnt < sorted_ranges.size()) {
+        if (sorted_ids[id_cnt] < sorted_ranges[range_cnt][0]) {
+            ++id_cnt;
+        } else if (sorted_ids[id_cnt] > sorted_ranges[range_cnt][1]) {
+            ++range_cnt;
+        } else if (
+            (sorted_ids[id_cnt] >= sorted_ranges[range_cnt][0]) 
+            and (sorted_ids[id_cnt] <= sorted_ranges[range_cnt][1])
+        ) {
+            ++num_valid_ids;
+            ++id_cnt;
+        }
+    }
+
+    return num_valid_ids;
+}
+
+int64_t all_valid_ids(const std::vector<std::vector<int64_t>> & sorted_ranges) {
+    int64_t num_valid_ids = 0;
     
+    for (const auto & sr: sorted_ranges) {
+        num_valid_ids += sr[1] - sr[0] + 1;
+    }
+    
+    return num_valid_ids;
 }
 
 int main() {
-    std::string filepath = "../input/test05.txt";
-    std::vector<std::vector<int>> ranges;
-    std::vector<int> ids;
+    std::string filepath = "../input/input05.txt";
+    std::vector<std::vector<int64_t>> ranges;
+    std::vector<int64_t> ids;
 
     read_input(filepath, ranges, ids);
 
-    std::vector<std::vector<int>> combined_ranges;
+    std::vector<std::vector<int64_t>> combined_ranges;
     sort_combine_ranges(ranges, combined_ranges);
+    std::vector<int64_t> sorted_ids = sort_vector(ids);
 
-    std::vector<int> sorted_ids = sort_vector(ids);
+    int64_t num_valid_ids = filter_valid_ids(combined_ranges, sorted_ids);
+    int64_t num_all_valid_ids = all_valid_ids(combined_ranges);
 
-    // std::vector<int> range_starts;
-    // std::vector<size_t> idx(ranges.size());
-    // std::iota(idx.begin(), idx.end(), 0);
-    // for (const auto r: ranges)
-    // {
-    //     range_starts.push_back(r[0]);
-    // }
-
-    // std::println("{}, {}, {}", ranges, range_starts, idx);
-
-    // quicksort(range_starts, idx, 0, ranges.size() - 1);
-    // std::println("{}, {}, {}", ranges, range_starts, idx);
-
-    
-
-    // std::println("rolls removed in first iteration (part 1): {}", roll_cnts[0]);
-    // std::println("rolls removed in total (part 2): {}", tot_roll_cnts);
+    std::println("number of valid IDs (part 1): {}", num_valid_ids);
+    std::println("number of ALL valid IDs (part 2): {}", num_all_valid_ids);
 }
