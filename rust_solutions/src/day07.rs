2d149f5da8067be650bf8bd083974acad4d9811b
Alexander Miessen <alexandermiessen@gmail.com>
2026-06-11 17:28:00 +0200

    rust day7 part 2 (no python interface and tests yet)

--- rust_solutions/src/day07.rs
+++ rust_solutions/src/day07.rs
@@ -1,12 +1,10 @@
 use pyo3::prelude::*;
-use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[pyclass]
+#[derive(Clone)]
 pub struct Grid {
-    // OPTIONS: HashSet{(x, y)} or HashMap {y_coord: Vec[x_coords]}
-    splitter_coords: HashMap<usize, Vec<usize>>,
+    splitter_coords: Vec<(usize, Vec<usize>)>,
     init_beam: usize,
     xdim: usize,
     ydim: usize,
@@ -17,12 +15,11 @@ pub struct Grid {
 /// @param filepath: path to the input file containing long integers, one per line
 ///
 /// @return an instance of Grid
-#[pyfunction]
 pub fn read_input(filepath: &str) -> Grid {
     let file = File::open(filepath).expect("could not open file");
     let reader = BufReader::new(file);
 
-    let mut splitter_coords: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut splitter_coords: Vec<(usize, Vec<usize>)> = Vec::new();
     let mut init_beam: usize = 0;
     let mut xdim: usize = 0;
     let mut ydim: usize = 0;
@@ -42,7 +39,7 @@ pub fn read_input(filepath: &str) -> Grid {
             // only store coords of splitters if there are any.
             // Don't store empty vecs for rows without splitters
             if !x_coords.is_empty() {
-                splitter_coords.insert(y, x_coords);
+                splitter_coords.push((y, x_coords));
             }
         }
         ydim += 1;
@@ -56,33 +53,62 @@ pub fn read_input(filepath: &str) -> Grid {
     }
 }
 
-// Could also come the other way around and loop through all x-coords of previous beam
+// For part 1: Could also come the other way around and loop through all x-coords of previous beam
 // (initialized with the first entering beam) and then check if the x-coords are in the
 // next row of beam splitter coords
-pub fn count_beam_splits(grid_data: Grid) -> usize {
+
+// For part 2: Use dynamic programming to count how many possible paths the initial beam
+// could take through all splitters (see example below)
+pub fn count_beam_splits(grid_data: Grid) -> (usize, usize) {
     let mut cnt: usize = 0;
+    let xdim = grid_data.xdim;
 
-    let mut prev_beam_x: HashSet<usize> = HashSet::new();
-    let mut new_beam_x: HashSet<usize> = HashSet::new();
+    let mut beam_x_coords: Vec<bool> = vec![false; xdim];
+    beam_x_coords[grid_data.init_beam] = true;
 
-    prev_beam_x.insert(grid_data.init_beam);
+    // DP array to keep track of possible beam paths. initially there is one beam
+    let mut dp_timelines: Vec<usize> = vec![0; xdim];
+    dp_timelines[grid_data.init_beam] = 1;
 
-    for y in 1..grid_data.ydim {
-        let Some(x_coords) = grid_data.splitter_coords.get(&y) else {
-            continue;
-        };
+    for (_y, x_coords) in &grid_data.splitter_coords {
         for &x in x_coords {
-            if !(x == 0) && !(x == grid_data.xdim) && prev_beam_x.contains(&x) {
+            if x > 0 && x < xdim - 1 && beam_x_coords[x] {
                 // x - 1 < 0 not possible due to type usize
-                new_beam_x.insert(x - 1);
-                new_beam_x.insert(x + 1);
-                prev_beam_x.remove(&x);
+                beam_x_coords[x - 1] = true;
+                beam_x_coords[x + 1] = true;
+                beam_x_coords[x] = false;
+
+                let dp_val = dp_timelines[x];
+                dp_timelines[x-1] += dp_val;
+                dp_timelines[x+1] += dp_val;
+                dp_timelines[x] = 0;
+
                 cnt += 1;
             }
         }
-        prev_beam_x.extend(&new_beam_x);
-        new_beam_x.clear();
     }
 
-    cnt
+    (cnt, dp_timelines.iter().sum())
 }
+
+
+
+// Example DP for counting possible all beam paths:
+// .......S.......
+// .......|.......
+// ......1^1......
+// ......|.|......
+// .....1^2^2.....
+// .....|.|.|.....
+// ....1^3^3^1....
+// ....|.|.|.|....
+// ...1^4^331^1...
+// ...|.|.|||.|...
+// ..1^5^434^2^1..
+// ..|.|.|||.|.|..
+// .1^154^74.21^1.
+// .|.|||.||.||.|.
+// 1^2^a^b^b^211^1  a = 10, b = 11
+// |.|.|.|.|.|||.|
+// 
+// Total: 1 + 2 + 10 + 11 + 11 + 2 + 1 + 1 + 1 = 40
